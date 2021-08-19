e36f86edb5e64e21612ff49264ccd8a7353d4f94
unknown <Frank Berger@w11.i1.fm-berger.de>
2021-08-19 22:33:31 +0200

    added code from CO2 trafic light of HFT Stuttgart

--- getData.php
+++ getData.php
@@ -1,11 +1,23 @@
 <?php 
+ini_set("error_reporting", E_ALL);
 
 // This is just an example of reading server side data and sending it to the client.
 // It reads a json formatted text file and outputs it.
 
-$string = file_get_contents("sampleData.json");
-echo $string;
+//$string = file_get_contents("sampleData.json");
+//echo $string;
 
-// Instead you can query your database and parse into JSON etc etc
+$db = new SQLite3('measurements.db');
+
+echo "Sensor time;CO2 concentration;Temperature;Humidity\n";
+echo "YYYY-MM-DD HH:MM:SS+ZZ;ppm;degC;%\n";
+
+$result = $db->query("SELECT device_id, received_at, co2, temperature, humidity FROM measurements where device_id = 'franks-co2-sensor'");      
+
+while($row = $result->fetchArray()) {
+    echo "{$row['received_at']};{$row['co2']};{$row['temperature']};{$row['humidity']}\n";
+}
+
+
 
 ?>
